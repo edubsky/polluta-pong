1f9433a159844eb555eb2dbcf8675f6e53b888eb
Eoin Dubsky <eoin@sumofus.org>
2021-12-13 20:13:36 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-// ABOUT THIS MINI GAME
-// 
-// This is about dirty lobbying by corporations more concerned about short-term profits than, well, anything really.
-// 
-// You can read more about it here: https://pollutamotor.com
-// 
-// Game by https://twitter.com/eoin
 controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
     players = 2
     controller.player2.moveSprite(Player_2, 0, 100)
@@ -14,8 +7,9 @@ controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pres
     controller.player2.moveSprite(Player_2, 0, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    otherSprite.vx = 1.1 * otherSprite.vx
-    otherSprite.vy = -1.1 * otherSprite.vy
+    otherSprite.vx = -1.1 * otherSprite.vx
+    otherSprite.vy = 1.1 * otherSprite.vy
+    music.playTone(494, music.beat(BeatFraction.Half))
 })
 function intro_scene () {
     effects.blizzard.startScreenEffect()
@@ -174,81 +168,213 @@ function intro_scene () {
         `, SpriteKind.Player)
     other_car.setPosition(106, 105)
     story.setPagePauseLength(1000, 2000)
-    story.spriteSayText(other_car, "WE NEED YOU TO STOP PESKY GREEN LAWS INTERFERING WITH OUR POLLUTING PLANS.", 15, 1, story.TextSpeed.Slow)
-    story.spriteSayText(other_car, "CAN YOU BAT THEM AWAY FOR US?", 15, 1, story.TextSpeed.Slow)
+    story.spriteSayText(other_car, "WE NEED YOU TO STOP PESKY GREEN LAWS INTERFERING WITH OUR POLLUTING PLANS.", 15, 1, story.TextSpeed.Normal)
+    story.spriteSayText(other_car, "CAN YOU BAT THEM AWAY FOR US?", 15, 1, story.TextSpeed.Normal)
     story.showPlayerChoices("NO PROBLEM!", "NO WAY!")
     if (story.checkLastAnswer("NO WAY!")) {
         game.over(true, effects.hearts)
     }
     pause(500)
-    story.spriteSayText(player_car, "JUST ONE THING... WHAT'S THE PLAN WHEN THE CLIMATE BREAKS DOWN?", 15, 1, story.TextSpeed.Slow)
-    story.spriteSayText(other_car, "DON'T WORRY ABOUT IT. JUST BAT AWAY THOSE GREEN LAWS!", 15, 1, story.TextSpeed.Slow)
+    story.spriteSayText(player_car, "JUST ONE THING... WHAT ABOUT THE CLIMATE?", 15, 1, story.TextSpeed.Normal)
+    story.spriteSayText(other_car, "UH... JUST BAT AWAY THOSE GREEN LAWS!", 15, 1, story.TextSpeed.Normal)
     for (let value2 of sprites.allOfKind(SpriteKind.Player)) {
         value2.destroy()
     }
     effects.blizzard.endScreenEffect()
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.splash("Nice lobbying!", "You delayed or destroyed " + laws + " climate action laws.")
-    game.over(false)
-})
 let other_car: Sprite = null
 let foreground: Sprite = null
 let player_car: Sprite = null
 let background: Sprite = null
-let laws = 0
 let Player_2: Sprite = null
 let players = 0
 game.setDialogCursor(img`
     . . . . . . . . . . . . . . . . . 
+    . . . c c c c c c c . . . . . . . 
+    . . c 1 1 1 c 1 1 1 c . . . . . . 
+    . c 1 1 1 c 1 c 1 1 1 c . . . . . 
+    c 1 c 1 1 c 1 b 1 1 c 1 c . . . . 
+    c 1 1 c c c c c c c 1 1 c . . . . 
+    c 1 1 1 1 c 1 b 1 1 1 1 c . . . . 
+    c 1 1 1 1 c 1 c 1 1 1 1 c . . . . 
+    . c 1 1 1 c 1 c 1 1 1 b . . . . . 
+    . . c 1 1 1 c 1 1 1 b . . . . . . 
+    . . . c c c c c c c . . . . . . . 
     . . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 2 2 2 2 2 . . . . 
-    . . . . . 2 2 2 2 2 2 2 2 2 . . . 
-    . . . . 2 2 2 2 2 1 2 2 2 2 2 . . 
-    . 2 2 2 2 2 2 2 1 2 1 2 2 2 2 2 . 
-    2 2 2 2 2 2 2 1 2 2 2 1 2 2 2 2 2 
-    2 2 2 2 2 2 2 1 1 1 1 1 2 2 2 2 2 
-    2 2 2 2 2 2 2 1 2 2 2 1 2 2 2 2 2 
-    2 2 2 2 2 2 2 1 2 2 2 1 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . . . 2 2 2 2 . . . . 2 2 2 2 . . 
+    . . . . . . . . . . . . . . . . . 
     `)
-game.splash("POLLUTA PONG")
+game.splash("POLLUTA PONG", "The only way to win is not to play")
 intro_scene()
+let laws = 0
 players = 1
-let seaLevel = sprites.create(img`
-    . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 8 8 . . 8 8 . 
-    `, SpriteKind.Enemy)
-seaLevel.y = 100
-seaLevel.vy = -1
+let Picture = image.create(scene.screenWidth(), scene.screenHeight())
+for (let index = 0; index <= scene.screenHeight(); index++) {
+    if (index % 6 < 4) {
+        Picture.setPixel(scene.screenWidth() / 2, index, 1)
+    }
+}
+scene.setBackgroundImage(Picture)
 let Player_1 = sprites.create(assets.image`Player 1`, SpriteKind.Player)
-Player_1.setPosition(80, 5)
-controller.moveSprite(Player_1, 100, 0)
+Player_1.setPosition(8, 60)
+controller.moveSprite(Player_1, 0, 100)
 Player_1.setStayInScreen(true)
 Player_2 = sprites.create(assets.image`Player 2`, SpriteKind.Player)
-Player_2.setPosition(80, 115)
+Player_2.setPosition(152, 60)
 Player_2.setStayInScreen(true)
-let projectile = sprites.createProjectileFromSprite(assets.image`Ball`, seaLevel, randint(25, 50), randint(-50, -75))
+let projectile = sprites.createProjectileFromSprite(assets.image`Ball`, Player_1, randint(50, 75), randint(25, 50))
 projectile.x += 3
 projectile.setBounceOnWall(true)
-laws = 0
+projectile.setFlag(SpriteFlag.ShowPhysics, false)
+let thermo = sprites.create(img`
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    222
+    `, SpriteKind.Enemy)
+thermo.top = 130
+thermo.vy = -2
 game.onUpdate(function () {
-    if (projectile.bottom > Player_2.bottom) {
-        projectile.setPosition(Player_2.x, Player_2.y - 5)
-        projectile.setVelocity(randint(25, 50), randint(-75, -50))
-        laws += 1
-    } else if (projectile.top < Player_1.top) {
-        projectile.setPosition(Player_2.x, Player_2.y - 5)
-        projectile.setVelocity(randint(25, 50), randint(-75, -50))
+    if (projectile.x > scene.screenWidth() / 2 && players == 1) {
+        if (projectile.y > Player_2.y) {
+            Player_2.y += 2
+        } else {
+            Player_2.y += -2
+        }
+    }
+    if (thermo.top <= 0) {
+        game.splash("YOU MADE IT!", "You delayed " + laws + " climate action laws!")
+        for (let value2 of sprites.allOfKind(SpriteKind.Player)) {
+            value2.destroy()
+        }
+        game.over(false)
     }
 })
 game.onUpdate(function () {
-    if (projectile.y > scene.screenHeight() / 2 && players == 1) {
-        if (projectile.x > Player_2.x) {
-            Player_2.x += 2
-        } else {
-            Player_2.x += -2
-        }
+    if (projectile.x > Player_2.x) {
+        laws += 1
+        music.jumpUp.play()
+        projectile.setPosition(Player_2.x - 5, Player_2.y)
+        projectile.setVelocity(randint(-75, -50), randint(25, 50))
+    } else if (projectile.x < Player_1.x) {
+        laws += 1
+        music.jumpUp.play()
+        projectile.setPosition(Player_2.x - 5, Player_2.y)
+        projectile.setVelocity(randint(-75, -50), randint(25, 50))
     }
 })
